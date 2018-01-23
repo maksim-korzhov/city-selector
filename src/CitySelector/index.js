81a2684abd433013ec243492203e287547dab2fe
Maksim Korzhov <korzhov.m.s@yandex.ru>
2018-01-23 15:01:04 +0300

    Small refactoring.

--- src/CitySelector/index.js
+++ src/CitySelector/index.js
@@ -1,8 +1,45 @@
+import $ from "jquery";
+
 import './style.less';
 
 export default class CitySelector {
-    constructor(configObj)
-    {
-        console.log(configObj);
+    constructor(configObj) {
+        // Init props
+        this.chooseRegionNode = null;
+
+        // Show block with region info
+        this.infoBlock = $("#info");
+        this.infoBlock.show();
+
+        // Set config options
+        this.elementId = configObj.elementId;
+        this.regionsUrl = configObj.regionsUrl;
+        this.localitiesUrl = configObj.localitiesUrl;
+        this.saveUrl = configObj.saveUrl;
+
+        // Create button
+        this.createRegionsSelector();
+    }
+
+    createRegionsSelector() {
+        // Draw button
+        this.chooseRegionNode = $("<button>").attr("id", "chooseRegion").text("Выбрать регион");
+        $(`#${this.elementId}`).append(this.chooseRegionNode)
+
+        this.chooseRegionNode.on("click", () => {
+            fetch(this.regionsUrl)
+                .then(response => {
+                    response.json().then(data => console.log(data));
+                    return response.json();
+                })
+                .catch(() => {
+                    console.log("Не удалось получить список регионов");
+                });
+        });
+    }
+
+    destroy() {
+        // Hide block with region info
+        this.infoBlock.hide();
     }
 }
--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
+import $ from "jquery";
 import CitySelector from './CitySelector';
 
-/* Пример создания компонента: */
-const citySelector = new CitySelector({
-    elementId: 'citySelector',
-    regionsUrl: 'http://localhost:3000/regions',
-    localitiesUrl: 'http://localhost:3000/localities',
-    saveUrl: 'http://localhost:3000/selectedRegions'
-});
+class App {
+    constructor() {
+        this.citySelector = null;
+    }
+
+    createCitySelector() {
+        this.citySelector = new CitySelector({
+            elementId: 'citySelector',
+            regionsUrl: 'http://localhost:3000/regions',
+            localitiesUrl: 'http://localhost:3000/localities',
+            saveUrl: 'http://localhost:3000/selectedRegions'
+        });
+    }
+
+    destroyCitySelector() {
+        this.citySelector.destroy();
+        this.citySelector = null;
+    }
+
+    init() {
+        $("#createCitySelector").on("click", () => {
+            this.createCitySelector();
+        });
+
+        $("#destroyCitySelector").on("click", () => {
+            this.destroyCitySelector();
+        });
+    }
+}
+
+const AppInstance = new App();
+AppInstance.init();
